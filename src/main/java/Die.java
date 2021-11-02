a6fdfe80af4c8d40639d86c27de882032bdc2d85
Lucas Eiruff <ldeiruff@gmail.com>
2021-11-02 22:05:51 +0100

    Added die class

---
+++ src/main/java/Die.java
@@ -0,0 +1,23 @@
+public class Die {
+    private final int MAX_ROLL = 6;
+
+    private int value;
+
+    public Die() {
+        roll();
+    }
+
+    public int roll() {
+        value = (int)(Math.random() * MAX_ROLL + 1); // Generates random number between (0 : MAX_ROLL) inclusive
+        return value;
+    }
+
+    public int getLastRoll() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(value);
+    }
+}
