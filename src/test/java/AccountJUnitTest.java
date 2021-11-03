2a8a7b827f03bba57495b93a1efd3decff0b9024
Lucas Eiruff <ldeiruff@gmail.com>
2021-11-03 23:23:29 +0100

    Test account cant withdraw/deposit negative

--- src/test/java/AccountJUnitTest.java
+++ src/test/java/AccountJUnitTest.java
@@ -18,4 +18,18 @@ class AccountJUnitTest {
         assertFalse(account.withdraw(account.getBalance() + 1));
         assertFalse(account.deposit(-account.getBalance() + 1));
     }
+
+    @DisplayName("Cant desposit negative amounts")
+    @Test
+    void cantDepositNegativeAmounts() {
+        assertFalse(account.deposit(-1));
+        assertFalse(account.deposit(Integer.MIN_VALUE));
+    }
+
+    @DisplayName("Cant withdraw negative amounts")
+    @Test
+    void cantWithdrawNegativeAmounts() {
+        assertFalse(account.withdraw(-1));
+        assertFalse(account.withdraw(Integer.MIN_VALUE));
+    }
 }
