c658727232971cf772931bc2048769cd2cc03e85
Lucas Eiruff <ldeiruff@gmail.com>
2021-11-06 10:33:31 +0100

    Started work on fields

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Player;
 import gui_main.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        GUI gui = new GUI();
+        GUI_Player player = new GUI_Player("John",20);
+        GameBoard gameBoard = new GameBoard(new GUI_Car[]{player.getCar()});
     }
 }
