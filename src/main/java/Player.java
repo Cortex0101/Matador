877b7c88ae293021be484be89f984d7fe2cbfd67
AugustHjortholm <s215800@dtu.dk>
2021-11-10 08:19:26 +0100

    added method SubtractFieldValueFromAccount in Player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,13 +5,14 @@ public class Player {
     private final String name;
     private final Die die;
     private final Account account;
-    private final CarController car;
+    private final CarController carController;
+    private CarModel carModel;
 
     public Player(String name, int initialCapital, GUI_Field[] fields) {
         this.name = name;
         this.die = new Die();
         this.account = new Account(initialCapital);
-        this.car = new CarController(fields, new GUI_Player(this.name, this.account.getBalance()));
+        this.carController = new CarController(fields, new GUI_Player(this.name, this.account.getBalance()));
     }
 
     public Die getDie() {
@@ -19,7 +20,7 @@ public class Player {
     }
 
     public CarController getCar() {
-        return car;
+        return carController;
     }
 
     public String getName() {
@@ -28,6 +29,11 @@ public class Player {
 
     public Account getAccount() {
         return account;
+    }
+
+    //Called after player lands on any field (even fields like chance and free parking)
+    public void SubtractFieldValueFromAccount(){
+        account.withdraw(FieldModel.getFieldValue(carModel.getPosition()));
     }
 
     @Override
