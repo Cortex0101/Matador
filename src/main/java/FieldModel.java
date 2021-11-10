c67b1bc65fdd4476280f8e1cd433d8e7e181c889
AugustHjortholm <s215800@dtu.dk>
2021-11-10 10:40:11 +0100

    Moved FieldInfo from GameBoard to FieldModel

--- src/main/java/FieldModel.java
+++ src/main/java/FieldModel.java
@@ -1,5 +1,17 @@
+import gui_fields.*;
+import gui_main.GUI;
+
+import java.awt.*;
+
 public class FieldModel {
+    private GUI_Field[] fields;
+    private GUI gui;
+
+    public FieldModel(){
+        fields = new GUI_Field[24];
+    }
     public static int getFieldValue(int field){
+
         return switch (field) {
             case 1, 2, 4, 5 -> 1;
             case 7,8,10,11 -> 2;
@@ -8,5 +20,38 @@ public class FieldModel {
             case 22, 23 -> 5;
             default -> 0;
         };
+    }
+
+    public GUI_Field[] FieldInfo() {
+        int i = 0;
+        fields[i++] = new GUI_Start("GO", "2$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("BURGER JOINT",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("PIZZA HOUSE",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Chance("?", "Chance", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("CANDY STORE",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("ICE CREAM PARLOR",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Jail("VISITING", "Modtag kr. 200,-\nnår de passerer start","JAIL","TEST", Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("MUSEUM",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("lIBRARY",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Chance("?", "Chance", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("SKATE PARK",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("SWIMMING POOL",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Tax("PARKING", "", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("GAME ARCADE",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("MOVIE THEATRE",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Chance("?", "Chance", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("TOY STORE",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("PET STORE",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Jail("C:\\Users\\ldeir\\OneDrive\\Desktop\\MSP73320hi35g4i8cc53bd00002230f3i7bb8efh8f.gif", "JAIL", "", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("BOWLING ALLEY",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("THE ZOO",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Chance("?", "Chance", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
+        fields[i++] = new GUI_Street("PARK PLACE",getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+        fields[i] = new GUI_Street("BOARDWALK",getFieldValue(i)+"$", "Modtag kr. 200,-\nnår de passerer start", FieldModel.getFieldValue(i - 1) + "$",Color.RED, Color.BLACK);
+
+        return fields;
+    }
+    public GUI_Field getField (int fieldNo) {
+        return (fields[fieldNo]);
     }
 }
