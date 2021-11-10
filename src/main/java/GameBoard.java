25512061599488b60f222160c965bb2709624416
AugustHjortholm <s215800@dtu.dk>
2021-11-10 22:09:50 +0100

    Added a primitive version of the game control, primarily for testing

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,27 +1,33 @@
 import gui_fields.*;
 import gui_main.GUI;
 
-import java.awt.*;
-import java.lang.reflect.Field;
+import java.util.Scanner;
 
 public class GameBoard {
-    private GUI_Field[] fields;
+    private FieldController fieldController;
+    private FieldModel fieldModel;
     private GUI_Car[] playerCars;
-    private GUI gui;
-    private FieldController field;
+    private Die die;
+    private Player[] players;
+    private GameBoardCreator gameBoardCreator;
+    private int activePlayer;
+    private boolean gameRunning;
 
     public GameBoard() {
-        this.field = new FieldController();
-        PlayerCreator players = new PlayerCreator(gui);
-        System.out.println(players.getPlayerCount());
-        System.out.println(players.getPlayerNames()[0]);
-        System.out.println(players.getPlayerNames()[1]);
+        this.gameBoardCreator = new GameBoardCreator();
+        this.players = gameBoardCreator.getPlayers();
+        this.fieldModel = new FieldModel();
+        this.fieldController = new FieldController();
+        this.gameRunning = true;
+        Scanner scan = new Scanner(System.in);
+        gameBoardCreator.getGUI().getUserButtonPressed("","Roll dice");
 
-        Player player = players.getPlayers()[0];
-        player.getCar().moveCar(25);
-        System.out.println(player.getCar().hasPassedStart());
-        player.getCar().moveCar(1);
-        System.out.println(player.getCar().hasPassedStart());
+        while(gameRunning) {
+            int roll = players[activePlayer].getDie().roll();
+            players[activePlayer].getCar().moveCar(roll);
+            fieldController.landOnField(players[activePlayer].getCar().getCarPosition(), fieldModel.FieldInfo()[activePlayer]);
+            scan.next("");
+        }
     }
 
     @Override
