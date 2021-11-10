9ca8c8867415dfc847a203af78a3dc473d552b0d
AugustHjortholm <s215800@dtu.dk>
2021-11-10 10:40:20 +0100

    Moved FieldInfo from GameBoard to FieldModel

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -2,42 +2,17 @@ import gui_fields.*;
 import gui_main.GUI;
 
 import java.awt.*;
+import java.lang.reflect.Field;
 
 public class GameBoard {
     private GUI_Field[] fields;
     private GUI_Car[] playerCars;
     private GUI gui;
+    private FieldModel field;
 
     public GameBoard() {
-        fields = new GUI_Field[24];
-
-        int i = 0;
-        fields[i++] = new GUI_Start("GO", "2$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("BURGER JOINT", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("PIZZA HOUSE", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Chance("CHANCE", "", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("CANDY STORE", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("ICE CREAM PARLOUR", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("VISITING", "", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("MUSEUM", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("LIBRARY", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Chance("CHANCE", "Modtag: 200", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("SKATE PARK", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("SWIMMING POOL", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("PARKING", "", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("VIDEO GAME ARCADE", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("MOVIE THEATRE", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Chance("CHANCE", "", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("TOY STORE", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("PET STORE", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Jail("C:\\Users\\ldeir\\OneDrive\\Desktop\\MSP73320hi35g4i8cc53bd00002230f3i7bb8efh8f.gif", "JAIL", "", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("BOWLING ALLEY", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("THE ZOO", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Chance("CHANCE", "", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i++] = new GUI_Start("PARK PLACE", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-        fields[i] = new GUI_Start("BOARDWALK", FieldModel.getFieldValue(i-1)+"$", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
-
-        gui = new GUI(fields);
+        this.field = new FieldModel();
+        gui = new GUI(field.FieldInfo());
         PlayerCreator players = new PlayerCreator(gui);
         System.out.println(players.getPlayerCount());
         System.out.println(players.getPlayerNames()[0]);
