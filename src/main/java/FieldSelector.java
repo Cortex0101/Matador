5967e3e11f8634ac5355e8d69173fe68d484d485
Lucas Eiruff <ldeiruff@gmail.com>
2021-11-12 22:45:23 +0100

    Selected a field from a list of options

---
+++ src/main/java/FieldSelector.java
@@ -0,0 +1,35 @@
+import gui_fields.GUI_Field;
+import gui_main.GUI;
+
+public class FieldSelector {
+     GUI gui;
+
+     GUI_Field[] fieldOptions;
+     GUI_Field fieldSelected;
+     String[] fieldTitles;
+
+     public FieldSelector(GUI_Field[] options, GUI gui) { // Needs a reference to gui in order to get user selection
+          this.gui = gui;
+          this.fieldOptions = options;
+          this.fieldTitles = getFieldTitlesArray(options);
+     }
+
+     public GUI_Field getSelected() {
+          return fieldSelected;
+     }
+
+     public void getUserSelection() {
+          gui.getUserSelection("Which field would you like to move to?", fieldTitles);
+     }
+
+     /**
+      * Utility function to convert an array of fields to an array of string with the fields titles
+      */
+     static private String[] getFieldTitlesArray(GUI_Field[] fields) {
+          String[] titles = new String[fields.length];
+          for (int i = 0; i < fields.length; i++) {
+               titles[i] = fields[i].getTitle();
+          }
+          return titles;
+     }
+}
