b07e9d7da49f4c02ba4ed84325eb2a63144d5ab4
Lucas Eiruff <ldeiruff@gmail.com>
2021-11-12 16:08:37 +0100

    Uncommented code

--- src/main/java/FieldController.java
+++ src/main/java/FieldController.java
@@ -11,7 +11,7 @@ public class FieldController {
 
     public void landOnField(int position,GUI_Field field){
         switch (position) {
-            //case 1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20, 22, 23 -> landOnOwnable(position,field);
+            case 1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20, 22, 23 -> landOnOwnable(position,field);
             case 3, 9, 15, 21 -> landOnChance();
             case 0, 6, 12 -> landOnFreeSpot();
             case 18 -> landOnGoToJail();
@@ -19,25 +19,22 @@ public class FieldController {
         }
 
     }
-    /*public void landOnOwnable(int position, GUI_Field field){
 
-                if (FieldModel.getFieldValue(position)>0){
-                    GUI_Street street = (GUI_Street) field;
-                    if (street.getOwnerName() != null) {
-                        landOnOwnedOwnable();
-                        System.out.println("Owned Space");
-                    }
-                    else
-                    {
-                        landOnUnownedOwnable();
-                        System.out.println("Unowned Space");
-                    }
-
-
-                }
+    public void landOnOwnable(int position, GUI_Field field){
+        if (FieldModel.getFieldValue(position)>0){
+            GUI_Street street = (GUI_Street) field;
+            if (street.getOwnerName() != null) {
+                landOnOwnedOwnable();
+                System.out.println("Owned Space");
+            }
+            else
+            {
+                landOnUnownedOwnable();
+                System.out.println("Unowned Space");
+            }
+        }
     }
 
-     */
     private void landOnUnownedOwnable(){}
     private void landOnOwnedOwnable(){}
     public void landOnChance(){
