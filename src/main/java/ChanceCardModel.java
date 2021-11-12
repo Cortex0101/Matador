7f21aad9ebd4a31f66ae6785b5106b932ec1a31d
Lucas Eiruff <ldeiruff@gmail.com>
2021-11-12 22:10:32 +0100

    ChanceCardModel base class

---
+++ src/main/java/ChanceCardModel.java
@@ -0,0 +1,11 @@
+public class ChanceCardModel {
+    private String text;
+
+    public ChanceCardModel(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
