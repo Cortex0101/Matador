a9dfe4ab784788aece069e50989c43d4dbcd765a
Lucas Eiruff <ldeiruff@gmail.com>
2021-11-14 10:08:13 +0100

    Test for pile

--- src/main/java/ChanceCardsPileController.java
+++ src/main/java/ChanceCardsPileController.java
@@ -1,5 +1,20 @@
-public class ChanceCardsPileController {
-    public ChanceCardsPileController() {
+import java.util.*;
 
+public class ChanceCardsPileController {
+    private Deque<ChanceCardController> chanceCards;
+
+    public ChanceCardsPileController(ChanceCardController[] chanceCardControllers) {
+        shufflePile(chanceCardControllers);
+        chanceCards = new ArrayDeque<>(Arrays.asList(chanceCardControllers));
+    }
+
+    public ChanceCardController drawCard() {
+        ChanceCardController ccc = this.chanceCards.pop();
+        this.chanceCards.addLast(ccc); // Place at the bottom of pile after drawing
+        return ccc;
+    }
+
+    public static void shufflePile(ChanceCardController[] chanceCardControllers) {
+        Collections.shuffle(Arrays.asList(chanceCardControllers));
     }
 }
---
+++ src/test/java/ChanceCardsPileControllerJUnitTest.java
@@ -0,0 +1,45 @@
+import gui_main.GUI;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChanceCardsPileControllerJUnitTest {
+    GUI gui;
+    int pileSize;
+    ChanceCardsPileController pile;
+
+    @BeforeEach
+    void setUp() {
+        gui = new GUI();
+        pileSize = 5;
+
+        ChanceCardController[] cardControllers = new ChanceCardController[]{
+                new ChanceCardController(new ChanceCardModel("A"), gui),
+                new ChanceCardController(new ChanceCardModel("B"), gui),
+                new ChanceCardController(new ChanceCardModel("C"), gui),
+                new ChanceCardController(new ChanceCardModel("D"), gui),
+                new ChanceCardController(new ChanceCardModel("E"), gui)
+        };
+
+        pile = new ChanceCardsPileController(cardControllers);
+    }
+
+    @Test
+    @DisplayName("Cards are in random order after shuffle")
+    void orderIsRandom() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("Cards loop when drawing")
+    void loops() {
+        var obj1 = pile.drawCard();
+        for (int i = 0; i < pileSize - 1; i++) {
+            pile.drawCard();
+        }
+        var obj2 = pile.drawCard();
+        assertEquals(obj1, obj2);
+    }
+}
