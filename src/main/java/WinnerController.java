bd06e57c0f7746f8a83e9eceb463d0a290b3037d
Lucas Eiruff <ldeiruff@gmail.com>
2021-11-17 12:46:52 +0100

    Winner can be detected counting field values

--- src/main/java/WinnerController.java
+++ src/main/java/WinnerController.java
@@ -1,3 +1,9 @@
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Street;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class WinnerController {
     Player[] players;
     FieldModel fieldModel;
@@ -7,15 +13,30 @@ public class WinnerController {
         this.fieldModel = fieldModel;
     }
 
-    public Player getPlayerWithMostValuableAccount() {
-        Player highestValuePlayer = players[0];
-        for (Player player : players) {
+    /***
+     * Note that if multiple palyers have equal balance AND equal property value, the last player in the array
+     * is picked as winner
+     * @return Winning player
+     */
+    public Player getWinner() {
+        Player highestAccountValuePlayer = getPlayerWithMostValuableAccount(this.players);
+        if (multiplePlayersWithBalanceOf(highestAccountValuePlayer.getAccount().getBalance())) {
+            Player[] mostValuablePlayers = getPlayersWithBalanceOf(highestAccountValuePlayer.getAccount().getBalance());
+            for (Player valuablePlayer : mostValuablePlayers) {
+                int propertyValue = getTotalPropertyValue(valuablePlayer);
+                Bank.payPlayer(valuablePlayer, propertyValue);
+            }
+            highestAccountValuePlayer = getPlayerWithMostValuableAccount(mostValuablePlayers);
+        }
+        return highestAccountValuePlayer;
+    }
+
+    private Player getPlayerWithMostValuableAccount(Player[] playerss) {
+        Player highestValuePlayer = playerss[0];
+        for (Player player : playerss) {
             if (player.getAccount().getBalance() > highestValuePlayer.getAccount().getBalance()) {
                 highestValuePlayer = player;
             }
-        }
-        if (multiplePlayersWithBalanceOf(highestValuePlayer.getAccount().getBalance())) {
-
         }
         return highestValuePlayer;
     }
@@ -29,7 +50,28 @@ public class WinnerController {
         return false;
     }
 
+    private Player[] getPlayersWithBalanceOf(int balance) {
+        ArrayList<Player> playersWithEqualBalance = new ArrayList<>();
+        for (Player player : players) {
+            if (player.getAccount().getBalance() == balance) {
+                playersWithEqualBalance.add(player);
+            }
+        }
+        return playersWithEqualBalance.toArray(new Player[0]);
+    }
+
     private int getTotalPropertyValue(Player player) {
-        return 0;
+        int totalValue = 0;
+        for (GUI_Field field : this.fieldModel.FieldInfo()) {
+            try {
+                GUI_Street street = (GUI_Street) field;
+                if (street.getOwnerName() == null) { continue; }
+                if (street.getOwnerName().equals(player.getName())) {
+                    totalValue += Character.getNumericValue(street.getRent().charAt(0));
+                }
+            }
+            catch (ClassCastException ignored) {}
+        }
+        return totalValue;
     }
 }
--- src/test/java/WinnerControllerJUnitTest.java
+++ src/test/java/WinnerControllerJUnitTest.java
@@ -1,4 +1,5 @@
 import gui_fields.GUI_Field;
+import gui_fields.GUI_Street;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,6 +26,59 @@ class WinnerControllerJUnitTest {
     @Test
     @DisplayName("Most valuable player is found with only 1 player having that balance")
     void singularMostValuablePlayer() {
-        assertEquals(winnerController.getPlayerWithMostValuableAccount(), players[1]);
+        assertEquals(winnerController.getWinner(), players[1]);
+    }
+
+    @Test
+    @DisplayName("Most valuable player is found with all players having equal balance and having to count property value")
+    void multipleMostValuablePlayers() {
+        // Set all players to have account of 10$
+        for (Player player : players) {
+            player.getAccount().withdraw(player.getAccount().getBalance());
+            player.getAccount().deposit(10);
+        }
+        // Except player 4, who has balance of 8.
+        players[3].getAccount().withdraw(2);
+
+        GUI_Street street1 = (GUI_Street) (fieldModel.FieldInfo()[1]);
+        GUI_Street street2 = (GUI_Street) (fieldModel.FieldInfo()[2]);
+        street1.setOwnerName(players[0].getName());
+        street2.setOwnerName(players[1].getName());
+
+        // Player 1 and 2 has balance of 11 total
+
+        GUI_Street street3 = (GUI_Street) (fieldModel.FieldInfo()[4]);
+        GUI_Street street4 = (GUI_Street) (fieldModel.FieldInfo()[5]);
+        street1.setOwnerName(players[2].getName());
+        street2.setOwnerName(players[2].getName());
+
+        // Player 3 has balance of 12 total
+
+        GUI_Street street5 = (GUI_Street) (fieldModel.FieldInfo()[7]);
+        GUI_Street street6 = (GUI_Street) (fieldModel.FieldInfo()[8]);
+        street5.setOwnerName(players[3].getName());
+        street6.setOwnerName(players[3].getName());
+
+        // Player 4 has balance of 14 total
+
+        GUI_Street street7 = (GUI_Street) (fieldModel.FieldInfo()[10]);
+        street7.setOwnerName(players[0].getName());
+
+        // Player 1 has balance of 13 total
+
+        assertEquals(players[0], winnerController.getWinner());
+
+        GUI_Street street = (GUI_Street) (fieldModel.FieldInfo()[13]);
+        street.setOwnerName(players[1].getName());
+
+        // Make sure to reset balances again as they were set by the previous call to getWinner()
+        for (Player player : players) {
+            player.getAccount().withdraw(player.getAccount().getBalance());
+            player.getAccount().deposit(10);
+        }
+        players[3].getAccount().withdraw(2);
+
+        // Player 2 has a balance of 14 total
+        assertEquals(players[1], winnerController.getWinner());
     }
 }
