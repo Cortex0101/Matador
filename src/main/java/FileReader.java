b0ccce0efd152de3ad17034b273f5873530d1667
AugustHjortholm <s215800@dtu.dk>
2021-11-18 17:54:45 +0100

    ReadFileText now properly removes unneeded text

--- src/main/java/FileReader.java
+++ src/main/java/FileReader.java
@@ -4,14 +4,15 @@ import java.io.FileNotFoundException;
 
 public class FileReader {
     private Scanner scan;
-    private File file;
+    private final String[] textTypeChecker;
 
     public FileReader() {
+        this.textTypeChecker = new String[]{"Title: ","Subtext: ","Description: ","Rent: ","Color: "};
         try{
-            this.file = new File("C:\\Users\\augus\\IdeaProjects\\03_part3\\src\\main\\resources\\field_info.txt");
+            File file = new File("C:\\Users\\augus\\IdeaProjects\\03_part3\\src\\main\\resources\\field_info.txt");
             this.scan = new Scanner(file);
         } catch (FileNotFoundException e) {
-            System.out.println("Am errpr occurred.");
+            System.out.println("Am error occurred.");
             e.printStackTrace();
         }
 
@@ -19,8 +20,10 @@ public class FileReader {
     public String[] ReadFileText(){
         String[] string = new String[5];
         for (int i = 0; i < string.length; i++) {
-            string[i] = scan.nextLine();
+                scan.skip(textTypeChecker[i]);
+                string[i] = scan.nextLine();
         }
+        scan.nextLine();
         return string;
     }
 }
