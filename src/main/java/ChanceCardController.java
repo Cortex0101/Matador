38f4defac68063eb431fd9f92f21309460cd4627
Lucas Eiruff <ldeiruff@gmail.com>
2021-11-20 15:19:43 +0100

    Removed GUI references from chance cards

--- src/main/java/ChanceCardController.java
+++ src/main/java/ChanceCardController.java
@@ -3,15 +3,13 @@ import gui_main.GUI;
 public class ChanceCardController {
     protected ChanceCardModel model;
     protected ChanceCardView view;
-    private GUI gui;
 
     public ChanceCardController(ChanceCardModel model, GUI gui) {
         this.model = model;
         this.view = new ChanceCardView(this.model.getText());
-        this.gui = gui;
     }
 
     public void onDraw() {
-        this.view.display(gui);
+        this.view.display();
     }
 }
