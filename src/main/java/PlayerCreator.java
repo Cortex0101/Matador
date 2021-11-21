336e228de338c4b3a98b7a955fa2c9de13f1d395
Lucas Eiruff <ldeiruff@gmail.com>
2021-11-21 22:25:28 +0100

    Simplified PlayerCreator

--- src/main/java/PlayerCreator.java
+++ src/main/java/PlayerCreator.java
@@ -2,25 +2,17 @@ import gui_main.GUI;
 
 public class PlayerCreator {
     private int playercount;
-    private String[] playernames;
     private Player[] players;
+
     public PlayerCreator(){
         playercount = GUIInstance.getInstance().getUserInteger("How many players are present",2,4);
-
-        playernames = new String[playercount];
-        for (int i = 0; i < playercount; i++) {
-            playernames[i] = GUIInstance.getInstance().getUserString("What shall player " + (i + 1) + " be called?");
-        }
-
         players = new Player[playercount];
         for (int i = 0; i < playercount; i++) {
-            players[i] = new Player(playernames[i], 20);
-        }
-
-        for (Player player : players) {
-            GUIInstance.getInstance().addPlayer(player.gui_player);
+            players[i] = new Player(GUIInstance.getInstance().getUserString("What shall player " + (i + 1) + " be called?"), 20);
+            GUIInstance.getInstance().addPlayer(players[i].gui_player);
         }
     }
+
     public Player[] getPlayers(){
         return players;
     }
