e0d9c100b5af6f20858e38b89e2f36893cab6969
AugustHjortholm <s215800@dtu.dk>
2022-01-04 03:34:40 +0100

    increased the initial captial for easier testing during development

--- src/main/java/PlayerCreator.java
+++ src/main/java/PlayerCreator.java
@@ -8,7 +8,8 @@ public class PlayerCreator {
         playercount = GUIInstance.getInstance().getUserInteger("How many players are present",2,4);
         players = new Player[playercount];
         for (int i = 0; i < playercount; i++) {
-            players[i] = new Player(GUIInstance.getInstance().getUserString("What shall player " + (i + 1) + " be called?"), 20);
+            //added more initial capital for development testing purposes
+            players[i] = new Player(GUIInstance.getInstance().getUserString("What shall player " + (i + 1) + " be called?"), 200000);
             GUIInstance.getInstance().addPlayer(players[i].gui_player);
         }
     }
