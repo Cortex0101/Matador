0819777370b2a5943d11ac14cb5151ec5d3e02b8
Cortex0101 <ldeiruff@gmail.com>
2022-01-04 09:40:06 +0100

    Added RaffleCup and made it possible to get previous dice roll from Die

---
+++ src/main/java/RaffleCup.java
@@ -0,0 +1,18 @@
+public class RaffleCup {
+    private Die die1;
+    private Die die2;
+
+    public RaffleCup() {
+        shake();
+    }
+
+    public int getEyes() {
+        return die1.getEyeCount() + die2.getEyeCount();
+    }
+
+    public int shake() {
+        die1.roll();
+        die2.roll();
+        return getEyes();
+    }
+}
