2beec6add0c8e4f5fa4ed7d38e35694c40f50f62
Cortex0101 <ldeiruff@gmail.com>
2022-01-05 12:04:06 +0100

    Added houses

--- src/main/java/StreetCard.java
+++ src/main/java/StreetCard.java
@@ -1,4 +1,6 @@
 public class StreetCard extends PropertyCard {
+    private int housePrice;
+    private int houseCount;
     public StreetCard(String name, int[] rents, int mortgageValue) {
         super(name, rents, mortgageValue);
     }
