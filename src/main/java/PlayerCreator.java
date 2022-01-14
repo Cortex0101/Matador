1972d90de6e6fc7ac1b26a35d9f42fdb992412a4
Cortex0101 <ldeiruff@gmail.com>
2022-01-14 09:19:58 +0100

    Added error checking to playerCount, and ability to enter 69

--- src/main/java/PlayerCreator.java
+++ src/main/java/PlayerCreator.java
@@ -16,7 +16,18 @@ public class PlayerCreator {
     private int getPlayerCountFromUser() {
         final int MIN_PLAYERS = 3;
         final int MAX_PLAYERS = 6;
-        return GUIInstance.getInstance().getUserInteger("How many players are present", MIN_PLAYERS, MAX_PLAYERS);
+        final int DEBUG_MODE = 69;
+        int playerCount;
+        do {
+            playerCount = GUIInstance.getInstance().getUserInteger("How many players are present", MIN_PLAYERS, MAX_PLAYERS);
+        } while (!(playerCount >= MIN_PLAYERS && playerCount <= MAX_PLAYERS) && playerCount != DEBUG_MODE);
+
+        if (playerCount == 69) {
+            // Setup debug mode
+            return 2; // debug mode will just have 2 players for now, as we can test everything we need to with just 2 players.
+        }
+
+        return playerCount;
     }
 
     public Player[] getPlayers(){
