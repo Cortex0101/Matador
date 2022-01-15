f5985703013a8690d7242bc4e25c28eb3d51fd93
AugustHjortholm <s215800@dtu.dk>
2022-01-15 18:18:34 +0100

    fixed auction so inactive players can't enter auctions

--- src/main/java/HandleAuction.java
+++ src/main/java/HandleAuction.java
@@ -7,7 +7,11 @@ public class HandleAuction {
     private int playerAmountStillBidding;
 
     public Player Auction(Player[] players) {
-        playerStillBidding = new boolean[players.length];
+        int disabledPlayers = 0;
+        for (int i = 0; i < disabledPlayers; i++) {
+            if (!players[i].getIsActive()){disabledPlayers++;}
+        }
+        playerStillBidding = new boolean[players.length-disabledPlayers];
         Arrays.fill(playerStillBidding, true);
         do {
             playerAmountStillBidding = 0;
@@ -18,8 +22,8 @@ public class HandleAuction {
             }
             for (int i = 0; i < playerStillBidding.length; i++) {
                 if (playerStillBidding[i] && playerAmountStillBidding>1){
-                    if (GUIInstance.getInstance().getUserSelection(players[i].getName() + ", choose to bid or pass", "bid", "pass").equals("bid")) {
-                        int bid = GUIInstance.getInstance().getUserInteger(players[i].getName() + ", Choose how much to bid");
+                    if (GUIInstance.getInstance().getUserSelection(players[i].getName() + ", choose to bid or pass.                             Current price: " + currentHighestbid, "bid", "pass").equals("bid")) {
+                        int bid = GUIInstance.getInstance().getUserInteger(players[i].getName() + ", Choose how much to raise the bid by \n Current price: "+ currentHighestbid);
                         while (bid > players[i].getAccount().getBalance()) {
                             bid = GUIInstance.getInstance().getUserInteger("you do not have enough money to bid that much. if you wish to pass, type 0 in the box");
                         }
